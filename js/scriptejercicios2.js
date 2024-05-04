578c9acd8dee4447e3c116287a22e7579cdded06
Hoodds <riberalander@gmail.com>
2024-05-04 21:53:02 +0200

    Hechos los 4 bloques de la pestaña 1. En DW se ven en row, en el navegador se ven en column. ¿?

--- js/scriptejercicios2.js
+++ js/scriptejercicios2.js
@@ -1,14 +1,16 @@
+// Función para abrir una pestaña específica
 function openTab(tabName) {
+	// Obtenemos todos los elementos con clase 'tabcontent' y los ocultamos
     var tabContents = document.getElementsByClassName("tabcontent");
     for (var i = 0; i < tabContents.length; i++) {
         tabContents[i].style.display = "none";
     }
-
+	// Obtenemos todos los elementos con clase 'tablink' y eliminamos la clase 'active'
     var tabLinks = document.getElementsByClassName("tablink");
     for (var i = 0; i < tabLinks.length; i++) {
         tabLinks[i].classList.remove("active");
     }
-
+	// Mostramos el contenido de la pestaña seleccionada y le añadimos la clase 'active' al botón correspondiente
     document.getElementById(tabName).style.display = "block";
     event.currentTarget.classList.add("active");
 }
