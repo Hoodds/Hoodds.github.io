37d43f8e164d85577b9cca3fc82dfa0859fb228d
Hoodds <riberalander@gmail.com>
2024-05-23 21:42:03 +0200

    galeria bajo el iframe ajustada.

--- js/script.js
+++ js/script.js
@@ -1,68 +1,23 @@
-// Espera a que el DOM esté completamente cargado
-    document.addEventListener('DOMContentLoaded', function() {
-        // Selecciona el elemento donde se mostrará la fecha y hora
-        var currentDateTimeElement = document.getElementById('currentDateTime');
+document.addEventListener('DOMContentLoaded', () => {
+  const thumbnailsContainer = document.querySelector('.thumbnails-container');
+  const thumbnails = document.querySelector('.thumbnails');
+  const fullImageOverlay = document.querySelector('.full-image-overlay');
+  const fullImage = document.querySelector('.full-image');
+  const closeBtn = document.querySelector('.close-btn');
 
-        // Actualiza la fecha y hora cada segundo
-        setInterval(function() {
-            // Obtiene la fecha y hora actual utilizando Day.js
-            var currentDateTime = dayjs().format('YYYY-MM-DD HH:mm:ss');
-
-            // Actualiza el contenido del elemento con la fecha y hora actual
-            currentDateTimeElement.textContent =/* 'Fecha y Hora: ' +*/ currentDateTime;
-        }, 1000); // Actualiza cada segundo
+  thumbnails.querySelectorAll('img').forEach((thumbnail) => {
+    thumbnail.addEventListener('click', () => {
+      openFullImage(thumbnail.src, thumbnail.alt);
     });
+  });
 
- document.addEventListener('DOMContentLoaded', function() {
-        // Define tu API key de OpenWeatherMap
-        var apiKey = 'caea1d5dc036daadff5aff8da540eac0';
+  function openFullImage(src, alt) {
+    fullImage.src = src;
+    fullImage.alt = alt;
+    fullImageOverlay.style.display = 'block';
+  }
 
-        // URL de la API de OpenWeatherMap para obtener datos meteorológicos de Bilbao
-        var city = 'Bilbao';
-        var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey + '&units=metric';
-
-        // Función para traducir las descripciones del tiempo
-        function traducirDescripcion(descripcion) {
-            // Mapea las descripciones del tiempo de inglés a español
-            var traducciones = {
-                'clear sky': 'cielo despejado',
-                'few clouds': 'pocas nubes',
-                'scattered clouds': 'nubes dispersas',
-                'broken clouds': 'nubes rotas',
-                'overcast clouds': 'nublado',
-                'light rain': 'lluvia ligera',
-                'moderate rain': 'lluvia moderada',
-                'heavy intensity rain': 'lluvia intensa',
-                'shower rain': 'lluvia',
-                'thunderstorm': 'tormenta',
-                'snow': 'nieve',
-                'mist': 'niebla'
-                // Puedes agregar más traducciones según sea necesario
-            };
-
-            // Devuelve la traducción correspondiente o la descripción original si no hay traducción
-            return traducciones[descripcion] || descripcion;
-        }
-
-        // Selecciona el elemento donde se mostrará el widget del tiempo
-        var weatherWidgetElement = document.getElementById('weatherWidget');
-
-        // Realiza una solicitud GET a la API de OpenWeatherMap
-        fetch(apiUrl)
-            .then(response => response.json())
-            .then(data => {
-                // Extrae la descripción del tiempo y temperatura actual
-                var weatherDescription = data.weather[0].description;
-                var temperature = data.main.temp;
-
-                // Traduce la descripción del tiempo
-                var descripcionTraducida = traducirDescripcion(weatherDescription);
-
-                // Actualiza el contenido del elemento con la información del tiempo en español
-                weatherWidgetElement.textContent =/* 'Tiempo en Bilbao: ' +*/ descripcionTraducida + ', Temperatura: ' + temperature + '°C';
-            })
-            .catch(error => {
-                console.error('Error al obtener datos meteorológicos:', error);
-                weatherWidgetElement.textContent = 'No se pudo obtener la información del tiempo para Bilbao';
-            });
-    });
+  closeBtn.addEventListener('click', () => {
+    fullImageOverlay.style.display = 'none';
+  });
+});
