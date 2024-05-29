1986b8d1e37140debd381ec438aedfc6f8572ffb
Hoodds <riberalander@gmail.com>
2024-05-29 15:00:38 +0200

    copiado el backup del dia 28 a la noche al DW

---
+++ js/script - Copia.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const thumbnailsContainer = document.querySelector('.thumbnails-container');
+    const thumbnails = document.querySelector('.thumbnails');
+    const fullImageOverlay = document.querySelector('.full-image-overlay');
+    const fullImage = document.querySelector('.full-image');
+    const closeBtn = document.querySelector('.close-btn');
+    const qrcodeSmall = document.getElementById('qrcode-small');
+    const qrcodeOverlay = document.getElementById('qrcode-overlay');
+    const qrcodeCloseBtn = document.getElementById('qrcode-close-btn');
+
+    qrcodeSmall.addEventListener('click', () => {
+        qrcodeOverlay.style.display = 'block';
+    });
+
+    qrcodeCloseBtn.addEventListener('click', () => {
+        qrcodeOverlay.style.display = 'none';
+    });
+
+    thumbnails.querySelectorAll('img').forEach((thumbnail) => {
+        thumbnail.addEventListener('click', () => {
+            const fullImageSrc = thumbnail.getAttribute('data-full');
+            openFullImage(fullImageSrc, thumbnail.alt);
+        });
+    });
+
+    function openFullImage(src, alt) {
+        fullImage.src = src;
+        fullImage.alt = alt;
+        fullImageOverlay.style.display = 'block';
+    }
+
+    closeBtn.addEventListener('click', () => {
+        fullImageOverlay.style.display = 'none';
+    });
+});
